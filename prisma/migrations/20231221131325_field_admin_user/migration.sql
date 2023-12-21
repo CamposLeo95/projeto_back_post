362ceac24f5f1b02387af5eb1b08b192082b562d
Leonardo Campos <leocampos.995@gmail.com>
2023-12-21 14:12:24 -0300

    criando migrations

---
+++ prisma/migrations/20231221131325_field_admin_user/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_users" ("email", "id", "name", "senha") SELECT "email", "id", "name", "senha" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;

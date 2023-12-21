c2aebb8188021aa61a3835540b339377db99d84c
Leonardo Campos <leocampos.995@gmail.com>
2023-12-21 14:15:01 -0300

    controller para posts

---
+++ src/controllers/PostController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express"
+import { PostService } from "../services/PostService"
+
+interface AuthRequest extends Request{
+    userId?: any 
+}
+
+class PostController {
+
+    private postService = new PostService()
+
+    async create (req: AuthRequest, res: Response) {
+        try {
+            const dataPosts = req.body
+            const {userId} = req.userId
+
+            if(!userId) {
+                return res.status(404).json({message: "Usuario não encontrado"})
+            }
+    
+            const result = await this.postService.create(dataPosts, userId)
+    
+            if(!result.post){
+               return res.status(result.status).json(result.message)
+            }
+    
+            return res.status(result.status).json({message: result.message, post: result.post})
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    
+    async list (req: Request, res: Response){
+        const result = await this.postService.list()
+
+        result && res.status(result.status).json(result.posts)
+    }
+}
+
+export { PostController }
---
+++ src/services/PostService.ts
@@ -0,0 +1,57 @@
+import { PrismaClient } from "@prisma/client"
+
+type postsProps = {
+    title: string
+    content: string
+}
+
+class PostService {
+
+    private prisma = new PrismaClient()
+
+    async create (dataPost: postsProps, idUser: number) {
+        try {
+            const { title, content } = dataPost
+
+            if(!title && !content){
+                return {status: 400, message: "Titulo ou conteudo pendente!"}
+            }
+
+            const user = await this.prisma.user.findUnique({where:{id: idUser}})
+
+            if(!user){
+                return {status: 404, message: "Usuario não cadastrado"}
+            }
+
+            const post = await this.prisma.post.create({
+                data: {
+                    title,
+                    content,
+                    userId: idUser
+                }
+            })
+            
+            if(!post){
+                return {status: 400, message: "Erro ao criar post"}
+            }
+
+            return {status: 201, message: "Post criado com sucesso", post}
+
+        } catch (error) {
+            throw error
+        }
+        
+    }
+
+    async list (){
+        const posts = await this.prisma.post.findMany({ include:{ user: true } })
+
+        if(!posts){
+            return {status:404, message: "nenhum post encontrado"}
+        }
+
+        return {status: 200, posts}
+    }
+}
+
+export { PostService }

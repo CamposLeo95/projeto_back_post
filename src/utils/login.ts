ef614dfe8836f3788ce9062bf22ef9fa52cd8796
Leonardo Campos <leocampos.995@gmail.com>
2023-12-21 14:15:28 -0300

    verificação do token

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,21 +1,20 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+interface AuthRequest extends Request{
+    userId?: string | JwtPayload
+}
 
 const prisma = new PrismaClient()
-
 const chaveSecreta = process.env.JWT_SECRET || 'chave_padrao_secreta';
 
 export async function login(req: Request, res: Response){
     const {email, senha} = req.body
 
     try { 
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
+        const user = await prisma.user.findUnique({ where: { email } })
     
         if(user && (await bcrypt.compare(senha, user.senha))){
             const token = jwt.sign({ userId: user.id }, chaveSecreta, { expiresIn: '1h' });
@@ -27,4 +26,22 @@ export async function login(req: Request, res: Response){
         throw error
     }
 
+}
+
+export async function verifyToken(req: AuthRequest, res: Response, next:() => void) {
+    const token = req.headers['authorization']
+
+
+    if(!token){
+        return res.status(403).json({message: 'Token não fornecido'})
+    }
+
+    jwt.verify(token, chaveSecreta, (err, decode) =>{
+        if(err || !decode){
+            return res.status(401).json({message: "Token invalido"})
+        }
+        req.userId = decode
+
+        next()
+    })
 }

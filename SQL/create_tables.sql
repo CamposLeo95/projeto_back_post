7f00a0d088f491e59484cd2e238af465687f87a4
Leonardo Campos <leocampos.995@gmail.com>
2025-03-24 11:57:38 -0300

    create tables SQL

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,20 +1,35 @@
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
 	email VARCHAR(100) NOT NULL UNIQUE,
+	name VARCHAR(100) NOT NULL,
 	password TEXT,
-	image_url TEXT,
-	bio TEXT
+	admin BOOLEAN DEFAULT false,
+	created_at TIMESTAMP DEFAULT NOW(),
+	updated_at TIMESTAMP DEFAULT NOW()
+	image_perfil TEXT,
+	image_cover TEXT,
+	bio VARCHAR(100)
 )
 
 CREATE TABLE posts(
 	id SERIAL PRIMARY KEY,
-	content_text TEXT NOT NULL,
+	content TEXT NOT NULL,
 	image_url TEXT,
 	id_user INT,
 	created_at TIMESTAMP DEFAULT NOW(),
 	FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE CASCADE
 )
+
+CREATE TABLE comments (
+	id SERIAL PRIMARY KEY,
+	content TEXT,
+	id_user INT,
+	id_post INT,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_post) REFERENCES posts(id) ON DELETE CASCADE
+);
+
 
 CREATE TABLE likes (
 	id_user INT,
@@ -24,12 +39,3 @@ CREATE TABLE likes (
 	FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE CASCADE,
 	FOREIGN KEY (id_post) REFERENCES posts(id) ON DELETE CASCADE
 )
-
-CREATE TABLE comments (
-	id SERIAL PRIMARY KEY,
-	content TEXT,
-	id_user INT,
-	id_post INT,
-	FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE CASCADE,
-	FOREIGN KEY (id_post) REFERENCES posts(id) ON DELETE CASCADE
-);
